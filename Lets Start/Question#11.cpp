28d62d89a18aa762376166fd26fe7bf772551b10
Muhammad Anas <137835308+m-anasahmed@users.noreply.github.com>
2024-06-03 23:38:20 +0500

    C++

--- Lets Start/Question#11.cpp
+++ Lets Start/Question#11.cpp
@@ -233,3 +233,42 @@ int main()
 //---------------------------------------------------------------------------------------------
 
 // Code 04
+// C++ implementation of the 
+// above approach
+#include <iostream>
+using namespace std;
+
+// Function to check palindrome
+int checkPalindrome(string str)
+{ 
+	// Calculating string length
+	int len = str.length();
+
+	// Traversing through the string 
+	// upto half its length
+	for (int i = 0; i < len / 2; i++) 
+	{	 
+		// Comparing i th character from 
+		// starting and len-i th character 
+		// from end
+		if (str[i] != str[len - i - 1])
+			return false;
+	}
+
+	// If the above loop doesn't return 
+	// then it is palindrome
+	return true;
+}
+
+// Driver Code
+int main()
+{ 
+	// Taking number as string
+	string st = "121";
+	if (checkPalindrome(st) == true)
+		cout << "Yes";
+	else
+		cout << "No";
+	return 0;
+}
+// This code is written by vikkycirus
